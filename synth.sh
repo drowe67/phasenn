5c196337794c758b780b59b587d2defca374d0da
David <david@rowetel.com>
2019-12-08 17:46:27 +1030

    adding vanilla codec2 as a comparison

--- synth.sh
+++ synth.sh
@@ -19,4 +19,6 @@ out_model=out.model
 sox -t .sw -r 8000 -c 1 $speech -t .sw - trim $st $len | c2sim - --modelout - | est_n0 -r > $base'_nolinear.model'
 ./phasenn_out.py $base'_nolinear.model' $nn $base'_out.model'
 sox -t .sw -r 8000 -c 1 $speech -t .sw - trim $st $len | c2sim - --modelout - | est_n0 -a $base'_out.model' > $base'_comb.model'
-sox -t .sw -r 8000 -c 1 $speech -t .sw - trim $st $len | c2sim - --modelin $base'_comb.model' -o $base'_out.raw'
+sox -t .sw -r 8000 -c 1 $speech -t .sw - trim $st $len | c2sim - --modelin $base'_comb.model' -o $base'_outnn.sw'
+sox -t .sw -r 8000 -c 1 $speech -t .sw - trim $st $len | c2sim - -o $base'_out.sw'
+sox -t .sw $base'_outnn.sw' -t .sw $base'_out.sw' $base'_both.sw'
